1fc4dc8ef693babcb4d698790aaf135741b53068
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2020-08-26 18:29:09 -0400

    refactor: add fill().with() helper

--- src/index.js
+++ src/index.js
@@ -13,26 +13,25 @@ function signedOutFlow() {
   document.querySelector('#signed-out-flow').style.display = 'block'
 }
 
+// A helper function to update DOM elements that have a "data-behavior" attribute
+// Search file for example use
+const fill = selector => ({
+  with: content =>
+    Array.from(document.querySelectorAll(`[data-behavior=${selector}]`))
+      .forEach(n => n.innerHTML = content)
+})
+
 // Displaying the signed in flow container and fill in data
 function signedInFlow() {
-  Array.from(document.querySelectorAll('[data-behavior=eth-node-url]'))
-    .forEach(n => n.innerHTML = 'eth-node-url')
-  Array.from(document.querySelectorAll('[data-behavior=eth-erc20-address]'))
-    .forEach(n => n.innerHTML = 'eth-erc20-address')
-  Array.from(document.querySelectorAll('[data-behavior=eth-erc20-abi-path]'))
-    .forEach(n => n.innerHTML = 'eth-erc20-abi-path')
-  Array.from(document.querySelectorAll('[data-behavior=eth-locker-address]'))
-    .forEach(n => n.innerHTML = 'eth-locker-address')
-  Array.from(document.querySelectorAll('[data-behavior=eth-locker-abi-path]'))
-    .forEach(n => n.innerHTML = 'eth-locker-abi-path')
-  Array.from(document.querySelectorAll('[data-behavior=near-node-url]'))
-    .forEach(n => n.innerHTML = 'near-node-url')
-  Array.from(document.querySelectorAll('[data-behavior=near-network-id]'))
-    .forEach(n => n.innerHTML = 'near-network-id')
-  Array.from(document.querySelectorAll('[data-behavior=near-fun-token-account]'))
-    .forEach(n => n.innerHTML = 'near-fun-token-account')
-  Array.from(document.querySelectorAll('[data-behavior=near-client-account]'))
-    .forEach(n => n.innerHTML = 'near-client-account')
+  fill('eth-node-url').with('eth-node-url')
+  fill('eth-erc20-address').with('eth-erc20-address')
+  fill('eth-erc20-abi-path').with('eth-erc20-abi-path')
+  fill('eth-locker-address').with('eth-locker-address')
+  fill('eth-locker-abi-path').with('eth-locker-abi-path')
+  fill('near-node-url').with('near-node-url')
+  fill('near-network-id').with('near-network-id')
+  fill('near-fun-token-account').with('near-fun-token-account')
+  fill('near-client-account').with('near-client-account')
 
   document.querySelector('#signed-in-flow').style.display = 'block'
 }
