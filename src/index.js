7e0abbab22085dcf88417590ff4209bac60e43bc
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2020-08-26 17:47:59 -0400

    fill in dummy data on sign in

--- src/index.js
+++ src/index.js
@@ -13,8 +13,27 @@ function signedOutFlow() {
   document.querySelector('#signed-out-flow').style.display = 'block'
 }
 
-// Displaying the signed in flow container and fill in account-specific data
+// Displaying the signed in flow container and fill in data
 function signedInFlow() {
+  Array.from(document.querySelectorAll('[data-behavior=eth-node-url]'))
+    .forEach(n => n.innerHTML = 'eth-node-url')
+  Array.from(document.querySelectorAll('[data-behavior=eth-erc20-address]'))
+    .forEach(n => n.innerHTML = 'eth-erc20-address')
+  Array.from(document.querySelectorAll('[data-behavior=eth-erc20-abi-path]'))
+    .forEach(n => n.innerHTML = 'eth-erc20-abi-path')
+  Array.from(document.querySelectorAll('[data-behavior=eth-locker-address]'))
+    .forEach(n => n.innerHTML = 'eth-locker-address')
+  Array.from(document.querySelectorAll('[data-behavior=eth-locker-abi-path]'))
+    .forEach(n => n.innerHTML = 'eth-locker-abi-path')
+  Array.from(document.querySelectorAll('[data-behavior=near-node-url]'))
+    .forEach(n => n.innerHTML = 'near-node-url')
+  Array.from(document.querySelectorAll('[data-behavior=near-network-id]'))
+    .forEach(n => n.innerHTML = 'near-network-id')
+  Array.from(document.querySelectorAll('[data-behavior=near-fun-token-account]'))
+    .forEach(n => n.innerHTML = 'near-fun-token-account')
+  Array.from(document.querySelectorAll('[data-behavior=near-client-account]'))
+    .forEach(n => n.innerHTML = 'near-client-account')
+
   document.querySelector('#signed-in-flow').style.display = 'block'
 }
 
