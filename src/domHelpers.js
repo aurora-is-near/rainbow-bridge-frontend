efbad7f2ddc0240385f05a206e57ed8f1193c18a
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2020-09-03 21:21:57 -0400

    feat(onsubmit): approve token

--- src/domHelpers.js
+++ src/domHelpers.js
@@ -1,3 +1,5 @@
+import BN from 'bn.js'
+
 // Update DOM elements that have a "data-behavior" attribute
 // Given `<span data-behavior="thing"></span>`
 // You can `fill('thing').with('whatever')` to set the innerHTML
@@ -49,4 +51,47 @@ export const initDOMhandlers = () => {
       }
     })
   })
+
+  document.querySelector('input#amount').oninput = (event) => {
+    const submitButton = document.querySelector('main form button')
+
+    if (event.target.value > 0) {
+      submitButton.disabled = false
+    } else {
+      submitButton.disabled = true
+    }
+  }
+
+  document.querySelector('main form').onsubmit = async (event) => {
+    event.preventDefault()
+
+    // get elements from the form using their id attribute
+    const { amount, fieldset, submit } = event.target.elements
+
+    // disable the form while the tokens get locked in Ethereum
+    fieldset.disabled = true
+
+    try {
+      // make an update call to the smart contract
+      await window.erc20.approve(
+        process.env.ethLockerAddress,
+        amount.value
+      )
+      // await transferErc20ToNear.lock(amount)
+    } catch (e) {
+      alert(
+        'Something went wrong! ' +
+        'Maybe you need to sign out and back in? ' +
+        'Check your browser console for more info.'
+      )
+      throw e
+    } finally {
+      // re-enable the form, whether the call succeeded or failed
+      fieldset.disabled = false
+    }
+
+    // if the call succeeded, reset the form
+    amount.value = 0
+    submit.disabled = true
+  }
 }
