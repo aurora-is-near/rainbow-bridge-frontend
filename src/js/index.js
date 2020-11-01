aurora-is-near/rainbow-bridge-frontend

commit 43ac0b454b2523b8380c05509b5988b4adef9548

fix: mv NEP21 Contract init to call site
This was missed in ea9de4c457d49873f63a6858340447fb063d6623

The NEP21 contract is no longer global, but depends on the specific
ERC20 you have selected.

src/html/erc20-to-near.html
@@ -125,7 +125,7 @@
         await window.initiateTransfer({
           amount: amount.value,
           callback: window.render,
-          erc20: getParam('erc20')
+          erc20: window.getParam('erc20')
         })
       } catch (e) {
         alert(
@@ -149,7 +149,7 @@
     }
     document.querySelector('[data-behavior=notBridged] button').onclick = function bridgeIt () {
       window.nearFungibleTokenFactory.deploy_bridge_token(
-        { address: getParam('erc20').replace('0x', '') },
+        { address: window.getParam('erc20').replace('0x', '') },
 
         // Default gas limit used by near-api-js is 3e13, but this tx fails with
         // that number. Doubling it works. Maybe slightly less would also work,
@@ -212,7 +212,7 @@
   async function getErc20Balance () {
     const erc20Contract = new window.web3.eth.Contract(
       JSON.parse(process.env.ethErc20AbiText),
-      getParam('erc20'),
+      window.getParam('erc20'),
       { from: window.ethUserAddress }
     )
 
@@ -222,8 +222,20 @@
   }
 
   async function getNep21Balance () {
-    return window.nep21
-      .get_balance({ owner_id: window.nearUserAddress })
+    const nep21Address =
+      window.getParam('erc20').replace('0x', '').toLowerCase() +
+      '.' +
+      process.env.nearTokenFactoryAccount
+
+    const nep21 = await new window.NearContract(
+      window.nearConnection.account(),
+      nep21Address,
+      {
+        viewMethods: ['get_balance']
+      }
+    )
+
+    return nep21.get_balance({ owner_id: window.nearUserAddress })
       .then(raw => Number(raw))
       .catch(() => null)
   }

src/js/index.js
@@ -1,5 +1,5 @@
 import BN from 'bn.js'
-import { utils } from 'near-api-js'
+import { Contract as NearContract, utils } from 'near-api-js'
 import './authEthereum'
 import './authNear'
 import { fill, hide, initDOMhandlers, show } from './domHelpers'
@@ -19,6 +19,7 @@ window.getTransfers = getTransfers
 window.hide = hide
 window.humanStatusFor = humanStatusFor
 window.initiateTransfer = initiateTransfer
+window.NearContract = NearContract
 window.parseNearAmount = utils.format.parseNearAmount
 window.render = render
 window.show = show

src/js/authNear.js
@@ -3,7 +3,6 @@ import { Contract, keyStores, WalletConnection, Near } from 'near-api-js'
 import { checkStatuses as checkTransferStatuses } from './transfers'
 import EthOnNearClient from './borsh/ethOnNearClient'
 import render from './render'
-import { get as getParam } from './urlParams'
 
 // Create a Near config object
 const near = new Near({
@@ -43,24 +42,11 @@ async function login () {
     }
   )
 
-  const nep21Address =
-    getParam('erc20').replace('0x', '').toLowerCase() +
-    '.' +
-    process.env.nearTokenFactoryAccount
-
-  window.nep21 = await new Contract(
-    window.nearConnection.account(),
-    nep21Address,
-    {
-      // View methods are read only
-      viewMethods: ['get_balance']
-    }
-  )
-
   window.ethOnNearClient = new EthOnNearClient(await new Contract(
     window.nearConnection.account(),
     process.env.nearClientAccount,
     {
+      // View methods are read only
       viewMethods: ['last_block_number']
     }
   ))

commit 6aee36dd313c02cc5d4f23d40e26204bc41c2da9

fix: undefined 'get_balance'
This bug was introduced in 43ac0b454b2523b8380c05509b5988b4adef9548

Since ea9de4c457d49873f63a6858340447fb063d6623, there's been no logical
global `window.nep21`, but `transfers` was still relying on one.

src/html/erc20-to-near.html
@@ -124,7 +124,7 @@
       fieldset.disabled = true
 
       try {
-        await window.initiateTransfer({
+        await window.transfers.initiate({
           amount: amount.value,
           callback: window.render,
           erc20: window.urlParams.get('erc20')
@@ -260,9 +260,7 @@
     const nep21 = await new window.NearContract(
       window.nearConnection.account(),
       nep21Address,
-      {
-        viewMethods: ['get_balance']
-      }
+      { viewMethods: ['get_balance'] }
     )
 
     return nep21.get_balance({ owner_id: window.nearUserAddress })

src/html/nav.html
@@ -74,7 +74,7 @@
       if (!clearTransferButton) return
 
       const transferId = clearTransferButton.closest('[data-behavior=transfer]').id
-      clearTransfer(transferId)
+      window.transfers.clear(transferId)
       render()
     })
 
@@ -87,7 +87,7 @@
       if (!retryTransferButton) return
 
       const transferId = retryTransferButton.closest('[data-behavior=transfer]').id
-      retryTransfer(transferId, render)
+      window.transfers.retry(transferId, render)
     })
   })
 
@@ -108,7 +108,7 @@
           <span>${'n' + transfer.erc20Name}</span>
         </header>
         <div>
-          <p>${window.humanStatusFor(transfer)}</p>
+          <p>${window.transfers.humanStatusFor(transfer)}</p>
         </div>
         ${inProgress ? '' : `
           <footer>
@@ -130,7 +130,7 @@
   }
 
   async function updateTransfers () {
-    const { inProgress, complete } = window.getTransfers()
+    const { inProgress, complete } = window.transfers.get()
 
     if (!inProgress.length && !complete.length) {
       show('transfers-none')

src/js/index.js
@@ -6,24 +6,18 @@ import { fill, hide, initDOMhandlers, show } from './domHelpers'
 import { getErc20Name } from './ethHelpers'
 import render from './render'
 import * as urlParams from './urlParams'
-import {
-  get as getTransfers,
-  initiate as initiateTransfer,
-  humanStatusFor
-} from './transfers'
+import * as transfers from './transfers'
 
 // Can't import modules in <script> tags in files included via PostHTML 😞
 window.BN = BN
 window.fill = fill
 window.getErc20Name = getErc20Name
-window.getTransfers = getTransfers
 window.hide = hide
-window.humanStatusFor = humanStatusFor
-window.initiateTransfer = initiateTransfer
 window.NearContract = NearContract
 window.parseNearAmount = utils.format.parseNearAmount
 window.render = render
 window.show = show
+window.transfers = transfers
 window.urlParams = urlParams
 
 initDOMhandlers()

src/js/transfers.js
@@ -24,8 +24,6 @@ import { getErc20Name } from './ethHelpers'
 //   coupling with the web3.js library. Make initialized library available here.
 // * window.tokenLocker: a web3.js `Contract` instance for the TokenLocker
 //   contract at address `process.env.ethLockerAddress`
-// * window.nep21: a near-api-js `Contract` instance for the `MintableFungibleToken`
-//   contract that mints a corresponding NEP21 token on NEAR
 // * window.ethOnNearClient: similar to a near-api-js `Contract` instance, but
 //   using a custom wrapper to handle Borsh serialization. See https://borsh.io
 //   and the code in ./authNear.js. This will be streamlined and added to
@@ -84,7 +82,7 @@ export async function checkStatuses (callback) {
     const transfer = getRaw()[minting]
     if (transfer) {
       const balanceAfter = Number(
-        await window.nep21.get_balance({ owner_id: window.nearUserAddress })
+        await getBalance.erc20ToNep21(transfer.erc20Address)
       )
       if (balanceAfter - transfer.amount === Number(balanceBefore)) {
         update(transfer, { status: COMPLETE, outcome: SUCCESS })
@@ -247,6 +245,28 @@ function initiateLock ({ erc20, amount }) {
   })
 }
 
+// Helpers to get balances for bridged tokens, organized in a way to attempt to
+// reduce confusion regarding which exact kind of token you're fetching the
+// balance for
+const getBalance = {
+  erc20ToNep21: async function getBridgedErc20Balance (erc20Address) {
+    const nep21Address =
+    erc20Address.replace('0x', '').toLowerCase() +
+    '.' +
+    process.env.nearTokenFactoryAccount
+
+    const nep21 = await new window.NearContract(
+      window.nearConnection.account(),
+      nep21Address,
+      { viewMethods: ['get_balance'] }
+    )
+
+    return nep21.get_balance({ owner_id: window.nearUserAddress })
+      .then(raw => Number(raw))
+      .catch(() => null)
+  }
+}
+
 // check the status of a single transfer
 // if `callback` is provided:
 //   * it will be called after updating the status in localStorage
@@ -351,7 +371,7 @@ async function checkStatus (id, callback) {
 // window.ethOnNearClient
 async function mint (transfer) {
   const balanceBefore = Number(
-    await window.nep21.get_balance({ owner_id: window.nearUserAddress })
+    await getBalance.erc20ToNep21(transfer.erc20Address)
   )
   urlParams.set({ minting: transfer.id, balanceBefore })
 
@@ -367,8 +387,8 @@ async function mint (transfer) {
   )
 }
 
-// Compute proof that Locked event was fired in Ethereum.
-// This proof can then be passed to window.nep21, which verifies the proof
+// Compute proof that Locked event was fired in Ethereum. This proof can then
+// be passed to the FungibleTokenFactory contract, which verifies the proof
 // against a Prover contract.
 async function findProof (transfer) {
   const receipt = await window.web3.eth.getTransactionReceipt(transfer.lockReceipt.transactionHash)
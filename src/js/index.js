6aee36dd313c02cc5d4f23d40e26204bc41c2da9
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2020-11-01 15:04:40 -0500

    fix: undefined 'get_balance'
    This bug was introduced in 43ac0b454b2523b8380c05509b5988b4adef9548
    
    Since ea9de4c457d49873f63a6858340447fb063d6623, there's been no logical
    global `window.nep21`, but `transfers` was still relying on one.

--- src/js/index.js
+++ src/js/index.js
@@ -6,24 +6,18 @@ import { fill, hide, initDOMhandlers, show } from './domHelpers'
 import { getErc20Name } from './ethHelpers'
 import render from './render'
 import * as urlParams from './urlParams'
-import {
-  get as getTransfers,
-  initiate as initiateTransfer,
-  humanStatusFor
-} from './transfers'
+import * as transfers from './transfers'
 
 // Can't import modules in <script> tags in files included via PostHTML 😞
 window.BN = BN
 window.fill = fill
 window.getErc20Name = getErc20Name
-window.getTransfers = getTransfers
 window.hide = hide
-window.humanStatusFor = humanStatusFor
-window.initiateTransfer = initiateTransfer
 window.NearContract = NearContract
 window.parseNearAmount = utils.format.parseNearAmount
 window.render = render
 window.show = show
+window.transfers = transfers
 window.urlParams = urlParams
 
 initDOMhandlers()
