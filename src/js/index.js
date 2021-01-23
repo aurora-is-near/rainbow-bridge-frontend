bfcd96178316f840845217371bebd253cc64abd1
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2021-01-22 21:51:46 -0500

    refactor: attach domHelpers exports to window.dom

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import BN from 'bn.js'
 import * as naj from 'near-api-js'
 import './authEthereum'
 import './authNear'
-import { find, findAll, fill, hide, initDOMhandlers, onClick, show, toString } from './domHelpers'
+import * as dom from './domHelpers'
 import render from './render'
 import * as urlParams from './urlParams'
 import * as transfers from './transfers'
@@ -11,18 +11,12 @@ import * as utils from './utils'
 
 // Can't import modules in <script> tags in files included via PostHTML 😞
 window.BN = BN
+window.dom = dom
 window.erc20Xnep21 = erc20Xnep21
-window.find = find
-window.findAll = findAll
-window.fill = fill
-window.hide = hide
 window.LOOP_INTERVAL = 5500
 window.NearContract = naj.Contract
-window.onClick = onClick
 window.parseNearAmount = naj.utils.format.parseNearAmount
 window.render = render
-window.show = show
-window.toString = toString
 window.transfers = transfers
 window.urlParams = urlParams
 window.utils = utils
