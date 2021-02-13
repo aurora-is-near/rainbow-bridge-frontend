aurora-is-near/rainbow-bridge-frontend

commit 9ea2a23a4279346575b34456af9d905c8949315b

refactor: nep21→nep141; mv deploy & getMetadata to lib
This started off as an effort to remove the
`window.nearFungibleTokenFactory` from `authNear` so that the extracted
library would only depend on the presence of `window.nearConnection`,
and ended with moving an enormous amount of code and renaming NEP21 to
NEP141.

The shape of the main exports from the connector client library
(`nep141~erc20`) has changed, and the documentation in that folder has
been updated accordingly.

src/js/transfers/nep141~erc20/natural-erc20/sendToNear/index.js
@@ -1,23 +1,22 @@
 import BN from 'bn.js'
 import getRevertReason from 'eth-revert-reason'
-import { Contract as NearContract } from 'near-api-js'
-import { getErc20Name } from '../../../utils'
-import * as urlParams from '../../../urlParams'
-import { stepsFor } from '../../i18nHelpers'
-import * as status from '../../statuses'
-import { track } from '../..'
+import getName from '../getName'
+import * as urlParams from '../../../../urlParams'
+import { stepsFor } from '../../../i18nHelpers'
+import * as status from '../../../statuses'
+import { track } from '../../..'
 import findProof from './findProof'
 import { lastBlockNumber } from './ethOnNearClient'
+import getNep141Balance from '../../bridged-nep141/getBalance'
 
 export const SOURCE_NETWORK = 'ethereum'
 export const DESTINATION_NETWORK = 'near'
+export const TRANSFER_TYPE = '@near~eth/nep141~erc20/natural-erc20/sendToNear'
 
-const last = arr => arr[arr.length - 1]
-
-const APPROVE = 'approve-natural-erc20-to-nep21'
-const LOCK = 'lock-natural-erc20-to-nep21'
-const SYNC = 'sync-natural-erc20-to-nep21'
-const MINT = 'mint-natural-erc20-to-nep21'
+const APPROVE = 'approve-natural-erc20-to-nep141'
+const LOCK = 'lock-natural-erc20-to-nep141'
+const SYNC = 'sync-natural-erc20-to-nep141'
+const MINT = 'mint-natural-erc20-to-nep141'
 
 const steps = [
   APPROVE,
@@ -95,7 +94,7 @@ export async function initiate ({
   recipient
 }) {
   // TODO: move to core 'decorate'; get both from contracts
-  const sourceTokenName = await getErc20Name(erc20Address)
+  const sourceTokenName = await getName(erc20Address)
   const destinationTokenName = sourceTokenName + 'ⁿ'
 
   // various attributes stored as arrays, to keep history of retries
@@ -110,9 +109,9 @@ export async function initiate ({
     sourceToken: erc20Address,
     sourceTokenName,
     status: status.ACTION_NEEDED,
-    type: '@eth+near/erc20+nep21/natural-erc20-to-nep21',
+    type: TRANSFER_TYPE,
 
-    // attributes specific to natural-erc20-to-nep21 transfers
+    // attributes specific to natural-erc20-to-nep141 transfers
     approvalHashes: [],
     approvalReceipts: [],
     completedConfirmations: 0,
@@ -254,7 +253,7 @@ async function checkSync (transfer) {
   }
 }
 
-// Mint NEP21 tokens to transfer.recipient. Causes a redirect to NEAR Wallet,
+// Mint NEP141 tokens to transfer.recipient. Causes a redirect to NEAR Wallet,
 // currently dealt with using URL params.
 async function mint (transfer) {
   console.log(transfer)
@@ -272,7 +271,10 @@ async function mint (transfer) {
   // able to correctly identify the transfer and see if the transaction
   // succeeded.
   setTimeout(async () => {
-    const balanceBefore = await getNep21Balance(transfer)
+    const balanceBefore = await getNep141Balance({
+      erc20Address: transfer.sourceToken,
+      user: transfer.recipient
+    })
     urlParams.set({ minting: transfer.id, balanceBefore })
     window.nearFungibleTokenFactory.deposit(
       proof,
@@ -301,7 +303,10 @@ export async function checkMint (transfer) {
   }
 
   const balanceBefore = Number(urlParams.get('balanceBefore'))
-  const balanceAfter = await getNep21Balance(transfer)
+  const balanceAfter = await getNep141Balance({
+    erc20Address: transfer.sourceToken,
+    user: transfer.recipient
+  })
 
   urlParams.clear('minting', 'balanceBefore')
 
@@ -326,19 +331,4 @@ export async function checkMint (transfer) {
   }
 }
 
-async function getNep21Balance (transfer) {
-  const nep21Address =
-    transfer.sourceToken.replace('0x', '').toLowerCase() +
-    '.' +
-    process.env.nearTokenFactoryAccount
-
-  const nep21 = await new NearContract(
-    window.nearConnection.account(),
-    nep21Address,
-    { viewMethods: ['get_balance'] }
-  )
-
-  return nep21.get_balance({ owner_id: transfer.recipient })
-    .then(raw => Number(raw))
-    .catch(() => null)
-}
+const last = arr => arr[arr.length - 1]

src/js/index.js
@@ -1,12 +1,10 @@
 import BN from 'bn.js'
 import * as naj from 'near-api-js'
-import './authEthereum'
-import './authNear'
 import * as dom from './domHelpers'
 import render from './render'
 import * as urlParams from './urlParams'
 import * as transfers from './transfers'
-import * as erc20Xnep21 from './transfers/erc20+nep21'
+import * as nep141Xerc20 from './transfers/nep141~erc20'
 import * as utils from './utils'
 
 dom.init()
@@ -14,7 +12,7 @@ dom.init()
 // Can't import modules in <script> tags in files included via PostHTML 😞
 window.BN = BN
 window.dom = dom
-window.erc20Xnep21 = erc20Xnep21
+window.nep141Xerc20 = nep141Xerc20
 window.LOOP_INTERVAL = 5500
 window.NearContract = naj.Contract
 window.parseNearAmount = naj.utils.format.parseNearAmount
@@ -26,3 +24,8 @@ window.utils = utils
 render()
 
 transfers.onChange(render)
+
+// These have side effects and may cause `render` calls which expect all
+// `window` additions above to be in place. Load last.
+require('./authEthereum')
+require('./authNear')

src/js/transfers/erc20+nep21/index.js
@@ -1,2 +1,0 @@
-export { initiate as naturalErc20ToNep21 } from './natural-erc20-to-nep21'
-export { initiate as bridgedNep21ToErc20 } from './bridged-nep21-to-erc20'

src/js/transfers/index.js
@@ -1,6 +1,6 @@
 // TODO find way to make getTransferType work without importing each of these
-import './erc20+nep21/bridged-nep21-to-erc20'
-import './erc20+nep21/natural-erc20-to-nep21'
+import './nep141~erc20/bridged-nep141/sendToEthereum'
+import './nep141~erc20/natural-erc20/sendToNear'
 import * as storage from './storage'
 import * as status from './statuses'
 
@@ -11,11 +11,11 @@ function getTransferType (transfer) {
     return require(transfer.type)
   } catch (depLoadError) {
     try {
-      return require(`./${transfer.type.split('/').slice(-2).join('/')}`)
+      return require(`./${transfer.type.split('/').slice(-3).join('/')}`)
     } catch (localLoadError) {
       console.error(depLoadError)
       console.error(localLoadError)
-      throw new Error(`Can't determine type for transfer: ${JSON.stringify(transfer)}`)
+      throw new Error(`Can't find library for transfer with type=${transfer.type}`)
     }
   }
 }

src/js/transfers/nep141~erc20/bridged-nep141/index.js
@@ -1,0 +1,5 @@
+export { default as getAddress } from './getAddress'
+export { default as getBalance } from './getBalance'
+export { default as getMetadata } from './getMetadata'
+export { default as deploy } from './deploy'
+export { initiate as sendToEthereum } from './sendToEthereum'

src/js/transfers/nep141~erc20/bridged-nep141/sendToEthereum/index.js
@@ -3,21 +3,22 @@ import bs58 from 'bs58'
 import getRevertReason from 'eth-revert-reason'
 import { toBuffer } from 'eth-util-lite'
 import { parseRpcError } from 'near-api-js/lib/utils/rpc_errors'
-import { getErc20Name } from '../../../utils'
-import * as status from '../../statuses'
-import { stepsFor } from '../../i18nHelpers'
-import { track } from '../..'
+import getErc20Name from '../../natural-erc20/getName'
+import * as status from '../../../statuses'
+import { stepsFor } from '../../../i18nHelpers'
+import { track } from '../../..'
 import { borshifyOutcomeProof } from './borshify-proof'
+import { userAuthedAgainst } from '../../utils'
+import getNep141Address from '../getAddress'
 
 export const SOURCE_NETWORK = 'near'
 export const DESTINATION_NETWORK = 'ethereum'
+export const TRANSFER_TYPE = '@near~eth/nep141~erc20/bridged-nep141/sendToEthereum'
 
-const last = arr => arr[arr.length - 1]
-
-const WITHDRAW = 'withdraw-bridged-nep21-to-erc20'
-const AWAIT_FINALITY = 'await-finality-bridged-nep21-to-erc20'
-const SYNC = 'sync-bridged-nep21-to-erc20'
-const UNLOCK = 'unlock-bridged-nep21-to-erc20'
+const WITHDRAW = 'withdraw-bridged-nep141-to-erc20'
+const AWAIT_FINALITY = 'await-finality-bridged-nep141-to-erc20'
+const SYNC = 'sync-bridged-nep141-to-erc20'
+const UNLOCK = 'unlock-bridged-nep141-to-erc20'
 
 const steps = [
   WITHDRAW,
@@ -81,15 +82,15 @@ export function checkStatus (transfer) {
 }
 
 export async function initiate ({
-  nep21Address,
+  erc20Address,
   amount,
   sender,
   recipient
 }) {
   // TODO: move to core 'decorate'; get both from contracts
-  const [erc20HexAddr] = nep21Address.split('.')
-  const destinationTokenName = await getErc20Name('0x' + erc20HexAddr)
+  const destinationTokenName = await getErc20Name(erc20Address)
   const sourceTokenName = destinationTokenName + 'ⁿ'
+  const sourceToken = getNep141Address(erc20Address)
 
   // various attributes stored as arrays, to keep history of retries
   const transfer = {
@@ -100,12 +101,12 @@ export async function initiate ({
     errors: [],
     recipient,
     sender,
-    sourceToken: nep21Address,
+    sourceToken,
     sourceTokenName,
     status: status.IN_PROGRESS,
-    type: '@eth+near/erc20+nep21/bridged-nep21-to-erc20',
+    type: TRANSFER_TYPE,
 
-    // attributes specific to natural-erc20-to-nep21 transfers
+    // attributes specific to bridged-nep141-to-erc20 transfers
     finalityBlockHeights: [],
     finalityBlockTimestamps: [],
     nearOnEthClientBlockHeight: null, // calculated & set to a number during checkSync
@@ -127,17 +128,11 @@ export async function initiate ({
   authenticate(transfer)
 }
 
-async function userAuthedAgainstTransferContract (transfer) {
-  const { accessKey } = await window.nearConnection.account().findAccessKey()
-  const authedAgainst = accessKey && accessKey.permission.FunctionCall.receiver_id
-  return authedAgainst === transfer.sourceToken
-}
-
 // current BridgeToken contract does not require deposit on `withdraw` function
 // call `requestSignIn` to add FunctionCall Access Key for this contract,
 // then once back at this app, can call `withdraw` without firing a redirect to NEAR Wallet.
 async function authenticate (transfer) {
-  if (!(await userAuthedAgainstTransferContract(transfer))) {
+  if (!(await userAuthedAgainst(transfer.sourceToken))) {
     // setTimeout to add access key for BridgeToken contract AFTER returning, so
     // that transfer can be updated in local storage and marked as no longer
     // failing (if needed)
@@ -157,7 +152,7 @@ async function authenticate (transfer) {
 
 async function withdraw (transfer) {
   // `authenticate` hasn't triggered redirect yet
-  if (!(await userAuthedAgainstTransferContract(transfer))) {
+  if (!(await userAuthedAgainst(transfer.sourceToken))) {
     return transfer
   }
   let withdrawTx
@@ -355,3 +350,5 @@ async function checkUnlock (transfer) {
     unlockReceipts: [...transfer.unlockReceipts, unlockReceipt]
   }
 }
+
+const last = arr => arr[arr.length - 1]

src/js/transfers/nep141~erc20/index.js
@@ -1,0 +1,2 @@
+export * as bridgedNep141 from './bridged-nep141'
+export * as naturalErc20 from './natural-erc20'

src/js/transfers/nep141~erc20/natural-erc20/index.js
@@ -1,0 +1,3 @@
+export { default as getMetadata } from './getMetadata'
+export { default as getName } from './getName'
+export { initiate as sendToNear } from './sendToNear'

commit 12965ae12ba700a743688ebf82a248a8d0aa9a01

fix: fix reference to undefined variable
fixes bug introduced in 9ea2a23a4279346575b34456af9d905c8949315b

src/js/transfers/nep141~erc20/natural-erc20/sendToNear/index.js
@@ -8,6 +8,7 @@ import { track } from '../../..'
 import findProof from './findProof'
 import { lastBlockNumber } from './ethOnNearClient'
 import getNep141Balance from '../../bridged-nep141/getBalance'
+import { checkNearAuth } from '../../utils'
 
 export const SOURCE_NETWORK = 'ethereum'
 export const DESTINATION_NETWORK = 'near'
@@ -256,7 +257,7 @@ async function checkSync (transfer) {
 // Mint NEP141 tokens to transfer.recipient. Causes a redirect to NEAR Wallet,
 // currently dealt with using URL params.
 async function mint (transfer) {
-  console.log(transfer)
+  await checkNearAuth(process.env.nearTokenFactoryAccount)
   const lockReceipt = last(transfer.lockReceipts)
   const proof = await findProof(lockReceipt.transactionHash)
 
@@ -276,7 +277,9 @@ async function mint (transfer) {
       user: transfer.recipient
     })
     urlParams.set({ minting: transfer.id, balanceBefore })
-    window.nearFungibleTokenFactory.deposit(
+    await window.nearConnection.account().functionCall(
+      process.env.nearTokenFactoryAccount,
+      'deposit',
       proof,
       new BN('300000000000000'),
       // We need to attach tokens because minting increases the contract state, by <600 bytes, which