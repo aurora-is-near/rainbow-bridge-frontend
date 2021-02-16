b200791c4133a846656c52a54bd085d7e6c82360
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2021-02-16 14:06:58 -0500

    refactor: rm unused logic in render.js

--- src/js/render.js
+++ src/js/render.js
@@ -2,20 +2,13 @@ import { fill } from './domHelpers'
 
 // update the html based on user & data state
 export default async function render () {
-  fill('ethErc20AbiText').with(process.env.ethErc20AbiText)
-  fill('ethLockerAddress').with(process.env.ethLockerAddress)
-  fill('ethLockerAbiText').with(process.env.ethLockerAbiText)
-  fill('nearNodeUrl').with(process.env.nearNodeUrl)
-  fill('nearNetworkId').with(process.env.nearNetworkId)
-  fill('nearFunTokenAccount').with(process.env.nearFunTokenAccount)
-  fill('nearClientAccount').with(process.env.nearClientAccount)
-
   if (window.ethUserAddress) {
     fill('ethUser').with({
       innerHTML: window.ethUserAddress,
       title: window.ethUserAddress
     })
   }
+
   if (window.nearUserAddress) {
     fill('nearUser').with({
       innerHTML: window.nearUserAddress,
