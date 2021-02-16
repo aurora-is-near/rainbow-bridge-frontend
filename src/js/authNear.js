3df78f56b7ddebd9d11888fea73af2fef1339459
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2021-02-16 15:23:16 -0500

    fix: NEAR sign out needs window.nearConnection

--- src/js/authNear.js
+++ src/js/authNear.js
@@ -17,18 +17,18 @@ const near = new Near({
 })
 
 // Initialize main interface to NEAR network
-const walletConnection = new WalletConnection(near)
+window.nearConnection = new WalletConnection(near)
 
-setNearConnection(walletConnection)
+setNearConnection(window.nearConnection)
 
 // Getting the Account ID. If still unauthorized, it's an empty string
-window.nearUserAddress = walletConnection.getAccountId()
+window.nearUserAddress = window.nearConnection.getAccountId()
 
 // Allow the current app to make calls to the specified contract on the
 // user's behalf. This works by creating a new access key for the user's
 // account and storing the private key in localStorage.
 onClick('authNear', () => {
-  walletConnection.requestSignIn(process.env.nearTokenFactoryAccount)
+  window.nearConnection.requestSignIn(process.env.nearTokenFactoryAccount)
 })
 
 function login () {
