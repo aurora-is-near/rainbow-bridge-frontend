1278bef31eeee541bc047fdc118b31ef60543a27
Pierre-Alain <ouvrard.pierre.alain@gmail.com>
2021-03-18 22:10:26 +0900

    build: add mainnet config

--- .config.js
+++ .config.js
@@ -76,5 +76,45 @@ module.exports = {
     nearExplorerUrl: 'https://explorer.testnet.near.org',
     nearNetworkId: 'testnet',
     ethNetworkId: 'rinkeby',
+  },
+  mainnet: {
+    // library settings
+    ethClientAddress: '0xce9d8c70c2ac161383c4debf207d884f6531b1b9',
+    ethNearOnEthClientAbiText: readFileSync('./abi/nearOnEthClient.abi'),
+    ethErc20AbiText: readFileSync('./abi/erc20.abi'),
+    ethLockerAbiText: readFileSync('./node_modules/rainbow-token-connector/res/BridgeTokenFactory.full.abi'),
+    ethLockerAddress: '0x23ddd3e3692d1861ed57ede224608875809e127f',
+    nearClientAccount: 'client.bridge.near',
+    nearHelperUrl: 'https://helper.near.org',
+    nearTokenFactoryAccount: 'factory.bridge.near',
+
+    // frontend settings
+    featuredErc20s: JSON.stringify([
+      '0xdac17f958d2ee523a2206206994597c13d831ec7', //   USDT
+      '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984', //   UNI
+      '0x514910771af9ca656af840dff83e8264ecf986ca', //   LINK
+      '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', //   USDC
+      '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', //   WBTC
+      '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9', //   AAVE
+      '0xa0b73e1ff0b80914ab6fe0444e65848c4c34450b', //   CRO
+      '0x50d1c9771902476076ecfc8b2a83ad6b9355a4c9', //   FTT
+      '0x4fabb145d64652a948d72533023f6e7a623c7c53', //   BUSD
+      '0x6f259637dcd74c767781e37bc6133cd6a68aa161', //   HT
+      '0x6b175474e89094c44da98b954eedeac495271d0f', //   DAI
+      '0x6b3595068778dd592e39a122f4f5a5cf09c90fe2', //   SUSHI
+      '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f', //   SNX
+      '0xc944e90c64b2c07662a292be6244bdf05cda44a7', //   GRT
+      '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2', //   MKR
+      '0xc00e94cb662c3520282e6f5717214004a7f26888', //   COMP
+      '0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e', //   YFI
+      '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', //   WETH
+      '0x0316eb71485b0ab14103307bf65a021042c6d380', //   HBTC
+    ]),
+    nearNodeUrl: 'https://rpc.near.org',
+    nearWalletUrl: 'https://wallet.near.org',
+    nearExplorerUrl: 'https://explorer.near.org',
+    nearNetworkId: 'mainnet',
+    ethNetworkId: 'main',
+
   }
 }
