5e08925781dfb7c1cedabf399868676af8d04984
Pierre-Alain <ouvrard.pierre.alain@gmail.com>
2021-05-02 19:22:05 +0900

    feat: Add eNEAR transfers.

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,4 +1,5 @@
 import { bridgedNep141, naturalErc20 } from '@near-eth/nep141-erc20'
+import { bridgedNEAR, naturalNEAR } from '@near-eth/near-ether'
 
 import { Decimal } from 'decimal.js'
 
@@ -29,12 +30,12 @@ export async function getErc20Data (address) {
   return { ...erc20, allowance, nep141 }
 }
 
-export async function getAllErc20s () {
+export async function getAllTokens () {
   const featuredErc20s = JSON.parse(process.env.featuredErc20s)
   let customErc20s = JSON.parse(localStorage.getItem(CUSTOM_ERC20_STORAGE))
   if (customErc20s === null) { customErc20s = [] }
 
-  return (await Promise.all(
+  const tokens = (await Promise.all(
     [...customErc20s, ...featuredErc20s].map(getErc20Data)
   )).reduce(
     (acc, token) => {
@@ -43,11 +44,30 @@ export async function getAllErc20s () {
     },
     {}
   )
+  return { ...tokens, near: await getNearData() }
+}
+
+export async function getNearData () {
+  const nearBalance = await naturalNEAR.getBalance(window.ethUserAddress)
+  const eNearBalance = await bridgedNEAR.getBalance(window.ethUserAddress)
+  return {
+    address: process.env.eNEARAddress,
+    balance: eNearBalance,
+    allowance: '-1',
+    decimals: 24,
+    name: 'eNEAR',
+    nep141: {
+      address: 'near',
+      balance: nearBalance,
+      name: 'NEAR'
+    }
+  }
 }
 
 export function rememberCustomErc20 (erc20Address) {
   erc20Address = erc20Address.toLowerCase()
   if (process.env.featuredErc20s.includes(erc20Address)) return
+  if (process.env.eNEARAddress === erc20Address) return
 
   const customErc20s = JSON.parse(localStorage.getItem(CUSTOM_ERC20_STORAGE))
   if (customErc20s === null) {
