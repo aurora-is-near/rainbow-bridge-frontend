e984fcf5d4769e178d677510775b64054120127e
Pierre-Alain <ouvrard.pierre.alain@gmail.com>
2021-05-14 12:17:49 +0900

    style: Fix MKR symbol

--- src/js/utils.js
+++ src/js/utils.js
@@ -22,6 +22,10 @@ export async function getErc20Data (address) {
     }),
     bridgedNep141.getMetadata(address, window.nearUserAddress)
   ])
+  if (address.toLowerCase() === '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2') {
+    erc20.name = 'MKR'
+    nep141.name = 'nMKR'
+  }
   return { ...erc20, allowance, nep141 }
 }
 
